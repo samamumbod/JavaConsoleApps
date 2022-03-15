9072a9b31edff11576e1eccd048141e253dd189c
samamumbod <samamumbod@gmail.com>
2022-03-15 14:13:49 +0100

    - implementation of screenshot program

--- src/main/java/com/apps/screenshort/Screenshot.java
+++ src/main/java/com/apps/screenshort/Screenshot.java
@@ -1,5 +1,32 @@
 package com.apps.screenshort;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
 public class Screenshot {
 
+    public static final long serialVersionUID = 1L;
+    public static void main(String[] args)
+    {
+        try {
+            Thread.sleep(120);
+            Robot r = new Robot();
+
+            // It saves screenshot to desired path
+            String path = "E:// Shot.jpg";
+
+            // Used to get ScreenSize and capture image
+            Rectangle capture =
+                    new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
+            BufferedImage Image = r.createScreenCapture(capture);
+            ImageIO.write(Image, "jpg", new File(path));
+            System.out.println("Screenshot saved");
+        }
+        catch (AWTException | IOException | InterruptedException ex) {
+            System.out.println(ex);
+        }
+    }
 }
